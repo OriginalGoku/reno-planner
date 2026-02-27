2e0facae4bcd2c19bb45530d5ebb53a87d03e6a6
Lord Goku <2144656@protonmail.com>
2026-02-27 17:15:57 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
+  ],
 };
 
 export default nextConfig;
